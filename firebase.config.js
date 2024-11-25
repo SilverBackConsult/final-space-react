5c5cf7645cc6b7d6376fdd94a1cf2d3ea8637634
Silver Back Consult <silverback.consult@outlook.com>
2024-11-25 11:57:40 +0000

    Installing Firebase

---
+++ firebase.config.js
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyDhQfwOg6x4417Z7cRGGTvFEPIH0GBZWio",
+  authDomain: "final-space-react-22dcb.firebaseapp.com",
+  projectId: "final-space-react-22dcb",
+  storageBucket: "final-space-react-22dcb.firebasestorage.app",
+  messagingSenderId: "96519849860",
+  appId: "1:96519849860:web:1a7b84745c2d968bd33fd1"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
